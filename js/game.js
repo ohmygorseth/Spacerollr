85c9b55d930f7f2e075de71eed073fc925cc964b
ohmygorseth <95774106+ohmygorseth@users.noreply.github.com>
2026-05-16 19:56:30 +0200

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -33,7 +33,7 @@ let nameInput='',enteringName=false;
 
 function reset(){camZ=0;px=0;pvx=0;jy=0;jvy=0;spd=CONFIG.BASE_SPEED;score=0;pts=[];rot=0;track=[];tBase=0;growTrack(0);state='play';}
 function die(){if(gameMode==='select'){state='dead';return;}if(score>hi)hi=score;if(isHighscore(score)){state='enter_name';nameInput='';enteringName=true;}else{state='dead';}}
-function go(){currentLevel=0;scoreOffset=0;reset();}
+function go(){if(gameMode==='select'){scoreOffset=999;reset();}else{currentLevel=0;scoreOffset=0;reset();}}
 function startMainMode(){gameMode='main';currentLevel=0;scoreOffset=0;menuState='main';reset();}
 function startLevel(n){gameMode='select';currentLevel=n;scoreOffset=999;menuState='play';reset();}
 function nextLevel(){
